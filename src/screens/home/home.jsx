b9cec22eeb9a1c79d6046ad09ed4eeccf2983deb
ferreirawill05 <william.ferreira@outlook.com.br>
2023-03-10 15:06:35 -0300

    main setup

---
+++ src/screens/home/home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function mainPage(){
+   
+    return(
+    
+        <>
+         
+        
+        </>
+
+    );
+    
+}
+ 
+
+
+
+
+
